f3962b16d0467484f50438c9a561a300d81ec72f
Lukas Grajewsky <grajewski.lukasz@wp.pl>
2019-10-15 21:02:16 +0200

    Fix no exist import

--- src/ClassGenerator/EvalClassGenerator.php
+++ src/ClassGenerator/EvalClassGenerator.php
@@ -1,11 +1,11 @@
 <?php
 namespace Collgus\GF\ClassGenerator;
 
+use Collgus\GF\ClassGenerator;
 use Collgus\GF\Content;
-use Collgus\GF\ClassWriter;
 use Throwable;
 
-final class EvalClassGenerator implements ClassWriter {
+final class EvalClassGenerator implements ClassGenerator {
     public function generate(Content $content): void {
         try {
             eval($content->toString());
