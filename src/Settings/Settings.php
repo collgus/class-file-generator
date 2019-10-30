3f16eaa99d88c71fbe8b0292c52616614286377b
git <dev3@melle.com>
2019-10-30 13:46:42 +0100

    Seetings fomrat fix

--- src/Settings/Settings.php
+++ src/Settings/Settings.php
@@ -1,16 +1,15 @@
-
 <?php
 namespace Collgus\GF\Settings;
 
-use Collgus\GF\ClassGenerator;
+use Collgus\GF\Generator;
+use Collgus\GF\Generator\FileGenerator;
 use Collgus\GF\Settings as CollgusSettings;
-use Collgus\GF\Generator\FileClassGenerator;
 
 class Settings implements CollgusSettings {
     public function getGeneratedClassPath(): string {
         return collgus_fg_path(["generated-classes"]);
     }
-    public function getClassGenerator(): ClassGenerator {
-        return new FileClassGenerator();
+    public function getClassGenerator(): Generator {
+        return new FileGenerator();
     }
 }
